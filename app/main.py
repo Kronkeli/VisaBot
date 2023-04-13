0d63334042aeb2386cebab4f494bc8e95226cf87
Samu Salko <samu.salko@gmail.com>
2023-04-13 14:03:25 +0300

    Lisää koodia

--- app/main.py
+++ app/main.py
@@ -21,6 +21,7 @@ conn = psycopg2.connect(
 )
 
 # Some global variables
+ACTIVE_ID = 0
 ACTIVE_QUESTION = "Mikä on neljä numeroissa?"
 ACTIVE_ANSWER = "4"
 
@@ -36,6 +37,31 @@ logging.basicConfig(
 def check_answer(guess):
     return guess == ACTIVE_ANSWER
 
+def query_question():
+    global ACTIVE_ID
+    global ACTIVE_QUESTION
+    global ACTIVE_ANSWER
+    global CURSOR
+    
+    CURSOR.execute("""SELECT * FROM quizzes WHERE id = %s""", (ACTIVE_ID,))
+    result_row = CURSOR.fetchone()
+    print(str(result_row))
+
+    ACTIVE_QUESTION = result_row(1)
+    ACTIVE_ANSWER = result_row(2)
+
+def reset_id():
+    global ACTIVE_ID
+    ACTIVE_ID = 0
+
+def add_id():
+    global ACTIVE_ID
+    ACTIVE_ID += 1
+
+def dec_id():
+    global ACTIVE_ID
+    ACTIVE_ID -= 1
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, MORO talk to me!")
 
@@ -46,6 +72,24 @@ async def send_question(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 async def send_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Lähettää aktiivisen vastauksen."""
     await update.message.reply_text(f"Aktiivinen vastaus on: {ACTIVE_ANSWER}")
+
+async def nullify_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Nollaa aktiivisen idn."""
+    reset_id()
+    global ACTIVE_ID
+    await update.message.reply_text(f"ID:tä nollattu ja se on nyt: {ACTIVE_ID}")
+
+async def increase_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Lisää aktiivista id:tä."""
+    add_id()
+    global ACTIVE_ID
+    await update.message.reply_text(f"ID:tä lisätty ja se on nyt: {ACTIVE_ID}")
+
+async def decrease_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Vähentää aktiviista id:tä."""
+    dec_id()
+    global ACTIVE_ID
+    await update.message.reply_text(f"ID:tä vähennetty ja se on nyt: {ACTIVE_ID}")
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Checks if the answer is the same as the active answer"""
@@ -65,9 +109,16 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     send_question_handler = CommandHandler('send_question', send_question)
     send_answer_handler = CommandHandler('send_answer', send_answer)
+    reset_id_handler = CommandHandler('send_answer', nullify_id)
+    add_id_handler = CommandHandler('send_answer', increase_id)
+    dec_id_handler = CommandHandler('send_answer', decrease_id)
+
     application.add_handler(start_handler)
     application.add_handler(send_question_handler)
     application.add_handler(send_answer_handler)
+    application.add_handler(reset_id_handler)
+    application.add_handler(add_id_handler)
+    application.add_handler(dec_id_handler)
 
     application.add_handler(MessageHandler(None, handle_message))
 
